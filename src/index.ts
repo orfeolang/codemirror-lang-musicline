bdb058aed9dd5b22e1b9db8d79b9e7bf885836e3
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2024-11-21 23:07:26 -0500

    Wrap up Musicline grammar and tests

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
-import { parser } from "./syntax.grammar"
-import { LRLanguage, LanguageSupport } from "@codemirror/language"
-import { styleTags, tags as t } from "@lezer/highlight"
+import { parser } from './syntax.grammar'
+import { LRLanguage, LanguageSupport } from '@codemirror/language'
+import { styleTags, tags as t } from '@lezer/highlight'
 
 export const MusiclineLanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
       styleTags({
-        LineComment: t.lineComment
-      })
-    ]
-  }),
-  languageData: {
-    commentTokens: {line: ";"}
-  }
+        LineComment: t.lineComment,
+        Timepoint: t.number,
+        Voice: t.integer,
+        EventType: t.typeName,
+        EventData: t.string,
+        NoteData: t.string,
+      }),
+    ],
+  })
 })
 
 export function Musicline() {
