84a93026eb1e88aa16e90773a060ba1db010f3f6
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2024-11-24 18:36:46 -0500

    Update test/test.js

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
-import {musiclineLanguage} from "../dist/index.js"
-import {fileTests} from "@lezer/generator/dist/test"
+import { musiclineLanguage } from "../dist/index.js"
+import { fileTests } from "@lezer/generator/dist/test"
 
 import * as fs from "fs"
 import * as path from "path"
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from 'url'
 let caseDir = path.dirname(fileURLToPath(import.meta.url))
 
 for (let file of fs.readdirSync(caseDir)) {
