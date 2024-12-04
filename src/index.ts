6d85b526a932e5d8c1e855f5bf1c0e64045ba177
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2024-12-03 21:25:45 -0500

    Update Musicline and OrfeoError syntaxes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { parser as musiclineParser } from './musicline.syntax.grammar'
-import { parser as orfeoParser } from './orfeo.syntax.grammar'
+// import { parser as orfeoParser } from './orfeo.syntax.grammar'
 import { parser as orfeoErrorParser } from './orfeoError.syntax.grammar'
 
 import { LRLanguage, LanguageSupport } from '@codemirror/language'
@@ -9,20 +9,23 @@ export const musiclineLanguage = LRLanguage.define({
   parser: musiclineParser.configure({
     props: [
       styleTags({
-        Timepoint:         t.number,
-        Voice:             t.integer,
-        EventMarkerId:     t.heading1,
-        EventNoteId:       t.heading2,
-        EventRestId:       t.heading3,
-        EventRestedId:     t.heading4,
-        EventTailId:       t.heading5,
-        EventTempoId:      t.heading6,
-        EventMarkerData:   t.string,
-        EventTempoData:    t.literal,
-        Escape:            t.escape,
-        EscapedNoteData:   t.list,
-        UnescapedNoteData: t.list,
-        LineComment:       t.lineComment,
+        LineComment:              t.lineComment,
+        Timepoint:                t.float,
+        Voice:                    t.integer,
+        EventMarkerId:            t.heading1,
+        EventNoteId:              t.heading2,
+        EventRestId:              t.heading3,
+        EventRestedId:            t.heading4,
+        EventTailId:              t.heading5,
+        EventTempoId:             t.heading6,
+        EventMarkerData:          t.labelName,
+        EventNoteEscapeChar:      t.escape,
+        EventNoteEscapedData:     t.string,
+        EventNoteUnescapedData:   t.string,
+        EventRestedEscapeChar:    t.character,
+        EventRestedEscapedData:   t.strikethrough,
+        EventRestedUnescapedData: t.strikethrough,
+        EventTempoData:           t.number,
       }),
     ],
   })
@@ -32,6 +35,7 @@ export function musicline() {
   return new LanguageSupport(musiclineLanguage)
 }
 
+/*
 export const orfeoLanguage = LRLanguage.define({
   parser: orfeoParser.configure({
     props: [
@@ -66,14 +70,16 @@ export function orfeo() {
   return new LanguageSupport(orfeoLanguage)
 }
 
+*/
+
 export const orfeoErrorLanguage = LRLanguage.define({
   parser: orfeoErrorParser.configure({
     props: [
       styleTags({
-        ErrorMessageMarker:     t.namespace,
-        CodeLineIndicator:      t.number,
+        ErrorMessageMarker:     t.className,
+        CodeLineIndicator:      t.integer,
         ErrorEjectIcon:         t.character,
-        ErrorSequenceStartIcon: t.keyword,
+        ErrorSequenceStartIcon: t.separator,
       }),
     ],
   })
